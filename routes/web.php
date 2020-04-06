75997e6b76fdfaf736b5e83a0c8a1841d09297dd
Juri <juri@mr-j.it>
2020-04-06 22:28:55 +0200

    Install routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +21,31 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+*/
+
+Auth::routes();
+
+Route::get('/', 'Dashboard@view')->name('home');
+
+Route::get('/service', 'Service@index')->name('service.list');
+Route::get('/service/create', 'Service@create')->name('service.create');
+Route::post('/service/store', 'Service@store')->name('service.store');
+Route::get('/service/edit/{id}', 'Service@edit')->name('service.edit');
+Route::post('/service/update/{id}', 'Service@update')->name('service.update');
+Route::get('/service/destroy/{id}', 'Service@destroy')->name('service.destroy');
+
+Route::get('/customer', 'Customer@index')->name('customer.list');
+Route::get('/customer/create', 'Customer@create')->name('customer.create');
+Route::post('/customer/store', 'Customer@store')->name('customer.store');
+Route::get('/customer/edit/{id}', 'Customer@edit')->name('customer.edit');
+Route::post('/customer/update/{id}', 'Customer@update')->name('customer.update');
+Route::get('/customer/destroy/{id}', 'Customer@destroy')->name('customer.destroy');
+Route::get('/customer/renew/{id}', 'Customer@renew')->name('customer.renew');
+
+Route::get('/setting', 'Setting@create')->name('setting.create');
+Route::post('/setting/store', 'Setting@store')->name('setting.store');
+
+Route::get('/list', 'Domain@get_list');
+Route::get('/mail/service-expiration', function (){
+    return view('mail.service-expiration');
+});
