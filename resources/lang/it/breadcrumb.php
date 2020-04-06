4938598dc1d8b0511d5d076ec15e6b3ee0813ded
Juri <juri@mr-j.it>
2020-04-06 23:40:56 +0200

    Install langs

---
+++ resources/lang/it/breadcrumb.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Breadcrumbs
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+	'' => 'Dashboard',
+	'login' => 'Login',
+	'service' => 'Servizi',
+    'customer' => 'Clienti',
+    'setting' => 'Impostazioni',
+
+    'create' => 'Nuovo',
+    'edit' => 'Modifica',
+
+    'name' => ':name',
+
+];
