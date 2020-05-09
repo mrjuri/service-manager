91ed280b7d008ba8aeaf58fe6b7ae937efe1254a
Juri <juri@mr-j.it>
2020-05-09 10:00:43 +0200

    payments table migrate

---
+++ database/migrations/2020_05_09_065001_payments.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Payments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->char('sid');
+            $table->integer('customer_service_id');
+            $table->timestamp('customer_service_expiration');
+            $table->char('type');
+            $table->timestamp('payment_date');
+            $table->float('amount');
+            $table->longText('services');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
