69415a3d97127bb5199294d58eb30e8e6681c20c
Juri <juri@mr-j.it>
2020-05-26 15:05:03 +0200

    guadagno trimestrale

--- resources/views/dashboard/tab/guadagno_per_mese.blade.php
+++ resources/views/dashboard/tab/guadagno_per_mese.blade.php
@@ -1,10 +1,64 @@
+<script type="text/javascript">
+
+    $(window).ready(function () {
+
+        $('td').hover(function () {
+
+            $Obj = $(this).closest('tr');
+
+            $Obj.find('td[rowspan]').addClass('hover');
+
+            if ($Obj.has('td[rowspan]').length == 0) {
+                $Obj.prevAll('tr:has(td[rowspan]):first').find('td[rowspan]').addClass('hover');
+            }
+
+        }, function () {
+
+            $Obj.find('td[rowspan]').removeClass('hover');
+            $Obj.prevAll('tr:has(td[rowspan]):first').find('td[rowspan]').removeClass('hover');
+
+        });
+
+        $('td[rowspan]').hover(function () {
+
+            $Obj = $(this);
+            $DataTrim = $Obj.data('trim');
+            $ObjRows = $Obj.closest('tbody').find('.' + $DataTrim).find('td');
+
+            $ObjRows.addClass('hover');
+
+        }, function () {
+
+            $ObjRows.removeClass('hover');
+
+        });
+
+    });
+
+</script>
+
+<style>
+    td {
+        transition: all .3s;
+    }
+    .hover {
+        background-color: #00ff00 !important;
+    }
+    td[rowspan].hover {
+        background-color: #00ff00 !important;
+        color: #000 !important;
+        font-weight: bold;
+    }
+</style>
+
 <table class="table table-hover">
     <thead>
     <tr>
-        <th>Mese</th>
-        <th class="text-right">Entrate</th>
-        <th class="text-right">Uscite</th>
-        <th class="text-right">Utile</th>
+        <th width="30%">Mese</th>
+        <th width="17.5%" class="text-right">Entrate</th>
+        <th width="17.5%" class="text-right">Uscite</th>
+        <th width="17.5%" class="text-right">Utile</th>
+        <th width="17.5%" class="text-center">U. Trimestre</th>
     </tr>
     </thead>
     <tbody>
@@ -24,7 +78,22 @@
             @php($className = 'table-warning text-dark')
         @endif
 
-        <tr class="{{ $className }}">
+        @if(!isset($m_count) || $m_count >= 3)
+
+            @if(isset($i))
+                @php($m_index = $i)
+            @else
+                @php($m_index = 1)
+            @endif
+
+            @php($m_count = 1)
+            @php($trim_tot = 0)
+
+        @else
+            @php($m_count += 1)
+        @endif
+
+        <tr class="{{ $className }} {{ 'trim_' . $m_index }}">
 
             <td>
                 {{ ucfirst($months_service['month']) }}
@@ -52,6 +121,22 @@
                     @endif
                 </strong>
             </td>
+
+            @if($m_count == 1)
+            <td rowspan="3" style="border-left: 1px solid #b3b7bb; background-color: #fff;" class="text-center align-middle" data-trim="{{ 'trim_' . $m_index }}">
+
+                @for($i = $m_index; $i <= ($m_index + 2); $i++)
+
+                    @if(isset($months_services[$i]['price_utile']))
+                        @php($trim_tot += number_format($months_services[$i]['price_utile'], 2, '.', ''))
+                    @endif
+
+                @endfor
+
+                &euro; {{ number_format($trim_tot, 2, ',', '.') }}
+
+            </td>
+            @endif
 
         </tr>
 
@@ -97,6 +182,7 @@
                 @endif
 
             </td>
+            <td></td>
         </tr>
     </tfoot>
 
